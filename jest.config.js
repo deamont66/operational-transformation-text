bc4d27ef50177aae1d5be8e8f8ef19d088e0f411
Jiří Šimeček <simecekjiri95@gmail.com>
2018-07-29 17:15:05 +0200

    Setup typescript project

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+const { defaults } = require("jest-config");
+
+module.exports = {
+  roots: ["<rootDir>/src/", "<rootDir>/tests/"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  moduleFileExtensions: [...defaults.moduleFileExtensions, "ts", "tsx"],
+  testURL: "http://localhost/" // cheat to fix SecurityError: localStorage is not available for opaque origins error see https://github.com/jsdom/jsdom/issues/2304
+};
